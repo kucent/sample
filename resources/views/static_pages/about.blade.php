8fb80870bfeca29e01c1b31939434376edc62691
Kucent <sincerlylsy@163.com>
2017-03-08 14:51:23 +0000

    Finish layout and routes

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.default')
+
+@section('content')
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      This is the about page.
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
+  </div>
+@stop
